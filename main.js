4ce78735929e894d92c5091d089ea88422812191
DrozdovAndrey <drozdov_andrey@list.ru>
2023-09-08 14:40:03 +0300

    add home work 3

---
+++ main.js
@@ -0,0 +1,7 @@
+function greeting(your_name){
+    // alert(`Привет, ${your_name}`);
+    console.log(`Привет, ${your_name}`);
+}
+
+const your_name = prompt('Введите ваше имя:');
+greeting(your_name); 
